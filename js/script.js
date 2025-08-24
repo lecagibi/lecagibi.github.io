4af3be5a55931946d68078ef1ef6172f3a67b6e5
lecagibi <kagibi@proton.me>
2025-08-24 19:01:44 +0200

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-/* charger la page a l'emplacement du scroll */
-
-window.addEventListener('scroll', () => {
-  const scrollable = document.documentElement.scrollHeight - window.innerHeight;
-  const scrolled = window.scrollY;
-});
-
