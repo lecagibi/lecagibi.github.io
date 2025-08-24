f92df75707b634e7250e9151de8d5576706d7e74
lecagibi <kagibi@proton.me>
2025-08-24 19:14:21 +0200

    Site-V1___GPush-2.2
    Site V1
    Grand Push 2.2

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* charger la page a l'emplacement du scroll */
+
+window.addEventListener('scroll', () => {
+  const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+  const scrolled = window.scrollY;
+});
+
